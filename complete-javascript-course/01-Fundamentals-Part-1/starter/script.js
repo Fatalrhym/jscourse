72b50de8ce239e5713e545e3f8fbeaff35cbe67f
Fatih <fyar87@gmail.com>
2023-08-26 12:34:39 +0300

    sec day commit

---
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,68 @@
+/*let js = "amazing";
+// console.log(40 + 50 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
+let person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let country = "Turkey";
+let continent = "Europe";
+let population = 85000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);*/
+
+
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun)
+
+//console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof "Joans");
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1987;
+
+console.log(year);
+
+console.log(typeof null);
+
+*/
+
+let age = 30;
+age = 31;
+
+const birthYear = 1987;
+//birthYear = 1988; can never change
+
+//const job = "cloud architect";
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "YAR"
+console.log(lastName);
