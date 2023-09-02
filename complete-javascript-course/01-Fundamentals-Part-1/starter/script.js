c4b6ba11769cd2eb13aae51cb73d48efea71a049
Fatih <fyar87@gmail.com>
2023-09-02 17:38:56 +0300

    last

--- complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -391,3 +391,62 @@ if (day === "monday") {
 }
 */
 
+/*
+//statement and expressions
+3 + 4
+191
+true && false && !false
+//exp
+
+if (23 > 10) {
+    const str = "23 is bigger";
+}
+//statement
+*/
+
+/*
+//The conditional(Ternary) Operator
+const age = 23;
+age >= 18 ? console.log("I like to drink wine🍷") : console.log("I like to drink water🥤"); //expression
+
+const drink = age >= 18 ? "wine🍷" : "water🥤"; //ternary opr
+console.log(drink);
+
+let drink2; // classic
+if (age >= 18) {
+    drink2 = "wine";
+} else {
+    drink2 = "water";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine🍷" : "water🥤"}`)
+*/
+
+/* 
+CHALLENGE #4
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+TEST DATA: Test with different bill values: 275, 40, and 430
+*/
+
+/*
+//SOLUTION
+const bill = 275;
+
+const tip = bill>50 && bill<300 ? (bill*15)/100 : bill*0.20;
+const total = bill+tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${total}`)
+*/
+
